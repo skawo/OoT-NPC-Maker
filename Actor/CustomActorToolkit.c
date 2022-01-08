d212ca543f1e81fd146842f7f71eeb6f34044760
skawo <43761362+skawo@users.noreply.github.com>
2022-01-08 05:28:26 +0100

    Create CustomActorToolkit.c

---
+++ Actor/CustomActorToolkit.c
@@ -0,0 +1,18 @@
+#define ZZROMTOOL 0
+#define LOG_TO_SCREEN 0
+#define LOGGING 0
+
+#include "source/npc_maker.c"
+#include "source/scripts.c"
+#include "source/update.c"
+#include "source/movement.c"
+#include "source/math.c"
+#include "source/init.c"
+#include "source/draw.c"
+#include "source/h_data.c"
+#include "source/h_debug.c"
+#include "source/h_doors.c"
+#include "source/h_movement.c"
+#include "source/h_rom.c"
+#include "source/h_scene.c"
+#include "source/h_scripts.c"
