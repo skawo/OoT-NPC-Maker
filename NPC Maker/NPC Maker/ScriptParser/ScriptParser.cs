f3f21108425b93ab389db2b9b6bd9bbf21a25f54
skawo <43761362+skawo@users.noreply.github.com>
2022-03-05 17:54:05 +0100

    Bugfix

--- NPC Maker/NPC Maker/ScriptParser/ScriptParser.cs
+++ NPC Maker/NPC Maker/ScriptParser/ScriptParser.cs
@@ -103,7 +103,7 @@ namespace NPC_Maker.Scripts
 
             ScriptText = Regex.Replace(ScriptText, @"([^>^<^=^\-^+^/^*^!])(=)([^=^\-^+^/^*^!^>^<])", m => m.Groups[1].Value + " " + m.Groups[2].Value + " " + m.Groups[3].Value);
             ScriptText = Regex.Replace(ScriptText, @"([^=])(<)([^=])", m => m.Groups[1].Value + " " + m.Groups[2].Value + " " + m.Groups[3].Value);
-            ScriptText = Regex.Replace(ScriptText, @"([^=])(>)([^=])", m => m.Groups[1].Value + " " + m.Groups[2].Value + " " + m.Groups[3].Value);
+            ScriptText = Regex.Replace(ScriptText, @"([^=^-])(>)([^=])", m => m.Groups[1].Value + " " + m.Groups[2].Value + " " + m.Groups[3].Value);
 
             ScriptText = Regex.Replace(ScriptText, @"\\\s+\n", "");                                                             // Override line carriage return if preceded by \
             ScriptText = ScriptText.Replace(",", " ").Replace("{", " ").Replace("}", " ").Replace("(", " ").Replace(")", " ");  // Remove ignored characters
