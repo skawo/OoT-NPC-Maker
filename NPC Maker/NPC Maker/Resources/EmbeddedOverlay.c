3a490a1b69d2b6d8cb0a538c7653368517aa6f4e
skawo <43761362+skawo@users.noreply.github.com>
2023-04-16 22:45:32 +0200

    Change EmbeddedOverlay.c template

--- NPC Maker/NPC Maker/Resources/EmbeddedOverlay.c
+++ NPC Maker/NPC Maker/Resources/EmbeddedOverlay.c
@@ -1,4 +1,9 @@
 #include <z64hdr/oot_mq_debug/z64hdr.h>
+
+// ^ Change the above to 
+// #include <z64hdr/oot_mq_debug/z64hdr.h>
+// if compiling for OoT 1.0
+
 #include <npcmaker/npc_maker_types.h>
 
 /*
