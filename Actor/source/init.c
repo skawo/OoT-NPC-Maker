bb14d3f0643d75308220f0aa943753d0ad02b029
skawo <43761362+skawo@users.noreply.github.com>
2024-04-05 07:47:36 +0200

    Update init.c

--- Actor/source/init.c
+++ Actor/source/init.c
@@ -205,12 +205,17 @@ static u8* Setup_LoadEmbeddedOverlay(NpcMaker* en, PlayState* playState, u8* buf
     int size = (uintptr_t)&ovl->relocations[ovl->nRelocations] - (uintptr_t)ovl;
     bzero(ovl, size);
 
+/*
+    Doesn't seem necessary?
+
     #if LOGGING == 1
         osSyncPrintf("_Clearing bss...");
     #endif
 
     if (ovl->bssSize != 0)
         bzero((void*)addr + len, ovl->bssSize);    
+
+*/
 
     #if LOGGING == 1
         osSyncPrintf("_Invalidating cache...");
