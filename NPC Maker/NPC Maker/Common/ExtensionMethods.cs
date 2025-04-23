e0956ff73afa7dc5f7cc2f1167c644336290df90
skawo <43761362+skawo@users.noreply.github.com>
2025-04-23 07:30:27 +0200

    whoops I just realized a bug

--- NPC Maker/NPC Maker/Common/ExtensionMethods.cs
+++ NPC Maker/NPC Maker/Common/ExtensionMethods.cs
@@ -43,7 +43,7 @@ namespace NPC_Maker
             var sb = new StringBuilder();
             foreach (char c in s)
             {
-                if (!char.IsPunctuation(c))
+                if (!char.IsPunctuation(c) || c == '\'')
                     sb.Append(c);
             }
             return sb.ToString();
