a6db7400d46a7dba61d20587e35b248030aad817
skawo <43761362+skawo@users.noreply.github.com>
2025-12-14 13:25:16 +0100

    Update LongInputBox.Designer.cs

--- NPC Maker/NPC Maker/Windows/LongInputBox.Designer.cs
+++ NPC Maker/NPC Maker/Windows/LongInputBox.Designer.cs
@@ -50,7 +50,7 @@ namespace NPC_Maker.Windows
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(410, 325);
+            this.button1.Location = new System.Drawing.Point(263, 325);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -61,7 +61,7 @@ namespace NPC_Maker.Windows
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(329, 325);
+            this.button2.Location = new System.Drawing.Point(182, 325);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -111,7 +111,7 @@ namespace NPC_Maker.Windows
             this.textBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
             this.textBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("textBox1.ServiceColors")));
             this.textBox1.ShowLineNumbers = false;
-            this.textBox1.Size = new System.Drawing.Size(473, 294);
+            this.textBox1.Size = new System.Drawing.Size(326, 294);
             this.textBox1.TabIndex = 3;
             this.textBox1.ToolTipDelay = 100;
             this.textBox1.UseCJK = FastColoredTextBoxNS.CJKMode.Hanzi;
@@ -123,7 +123,7 @@ namespace NPC_Maker.Windows
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 358);
+            this.ClientSize = new System.Drawing.Size(350, 358);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
