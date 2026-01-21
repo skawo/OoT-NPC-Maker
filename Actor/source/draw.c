406fb3608384535fab2a84b17e4657994929bf0c
skawo <43761362+skawo@users.noreply.github.com>
2026-01-21 23:48:06 +0100

    Revert "Minor performance improvement"
    This reverts commit 082815320366a76ad58edb48e3fe6b926c421217.

--- Actor/source/draw.c
+++ Actor/source/draw.c
@@ -512,11 +512,10 @@ s32 Draw_OverrideLimbDraw(PlayState* playState, s32 limbNumber, Gfx** dListPtr, 
         ColorEntry col = en->dListColors[i];
 
         if (sLimbNumber == col.limb)
-        {
             en->curColor = col.color;
-            Draw_SetEnvColor(gfxP, en->curColor, en->curAlpha);
-        }
     }
+
+    Draw_SetEnvColor(gfxP, en->curColor, en->curAlpha);
 
 #pragma endregion
 
