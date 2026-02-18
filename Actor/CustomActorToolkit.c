aab5597a88af20063b7d49eadde76d36f440aeb4
skawo <43761362+skawo@users.noreply.github.com>
2026-02-18 18:45:26 +0100

    Update CustomActorToolkit.c

--- Actor/CustomActorToolkit.c
+++ Actor/CustomActorToolkit.c
@@ -1,24 +1,26 @@
-#define ZZROMTOOL 0
-#define LOGGING 0
-#define EXDLIST_EDITOR 0
-#define DEBUG_STRUCT 0
-#define LOG_VERSION 0
-#define LOOKAT_EDITOR 0
-#define COLLISION_VIEWER 1
-#define DIRECT_ROM_LOAD 0
-#define GAME_VERSION 0
+#if !defined(NPCM_Z64ROM)
+    #define ZZROMTOOL 0
+    #define LOGGING 0
+    #define EXDLIST_EDITOR 0
+    #define DEBUG_STRUCT 0
+    #define LOG_VERSION 0
+    #define LOOKAT_EDITOR 0
+    #define COLLISION_VIEWER 1
+    #define DIRECT_ROM_LOAD 0
+    #define GAME_VERSION 0
 
-#include "source/npc_maker.c"
-#include "source/scripts.c"
-#include "source/update.c"
-#include "source/movement.c"
-#include "source/math.c"
-#include "source/init.c"
-#include "source/draw.c"
-#include "source/h_data.c"
-#include "source/h_doors.c"
-#include "source/h_movement.c"
-#include "source/h_rom.c"
-#include "source/h_scene.c"
-#include "source/h_scripts.c"
-#include "source/npc_maker_user.c"
+    #include "source/npc_maker.c"
+    #include "source/scripts.c"
+    #include "source/update.c"
+    #include "source/movement.c"
+    #include "source/math.c"
+    #include "source/init.c"
+    #include "source/draw.c"
+    #include "source/h_data.c"
+    #include "source/h_doors.c"
+    #include "source/h_movement.c"
+    #include "source/h_rom.c"
+    #include "source/h_scene.c"
+    #include "source/h_scripts.c"
+    #include "source/npc_maker_user.c"
+#endif
