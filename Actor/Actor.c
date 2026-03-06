8fc3145630345a8d3068532551f9add75e8e5415
skawo <43761362+skawo@users.noreply.github.com>
2026-03-06 10:13:35 +0100

    update main c for compilation with zlinker

---
+++ Actor/Actor.c
@@ -0,0 +1,27 @@
+#define ZZROMTOOL 0
+#define LOGGING 0
+#define EXDLIST_EDITOR 1
+#define DEBUG_STRUCT 1
+#define LOG_VERSION 1
+#define LOOKAT_EDITOR 1
+#define COLLISION_VIEWER 1
+#define DIRECT_ROM_LOAD 1
+#define GAME_VERSION 0
+
+#define NPCM_Z64ROM
+#define F3DEX_GBI_2
+
+#include "source/npc_maker.c"
+#include "source/scripts.c"
+#include "source/update.c"
+#include "source/movement.c"
+#include "source/math.c"
+#include "source/init.c"
+#include "source/draw.c"
+#include "source/h_data.c"
+#include "source/h_doors.c"
+#include "source/h_movement.c"
+#include "source/h_rom.c"
+#include "source/h_scene.c"
+#include "source/h_scripts.c"
+#include "source/npc_maker_user.c"
