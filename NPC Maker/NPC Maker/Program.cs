a2d3675316ffe17f99b2bcc696e46e8bad928d81
skawo <43761362+skawo@users.noreply.github.com>
2026-08-30 14:28:10 +0200

    Ignore this property entirely

--- NPC Maker/NPC Maker/Program.cs
+++ NPC Maker/NPC Maker/Program.cs
@@ -91,7 +91,7 @@ namespace NPC_Maker
         private static void PrintBanner()
         {
             ConsoleWriteLineS();
-            ConsoleWriteLineS($"Zelda Ocarina of Time NPC Creation Tool v.3.77 tempCLI dotNET");
+            ConsoleWriteLineS($"Zelda Ocarina of Time NPC Creation Tool v.3.771 tempCLI dotNET");
         }
 
         private static void InitializePaths()
