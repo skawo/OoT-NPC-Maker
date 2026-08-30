f6c8ccb53c75ae1e780e78cffc4eb77d7f2ba3b8
skawo <43761362+skawo@users.noreply.github.com>
2026-08-30 15:07:41 +0200

    Update Program.cs

--- NPC Maker/NPC Maker/Program.cs
+++ NPC Maker/NPC Maker/Program.cs
@@ -91,7 +91,7 @@ namespace NPC_Maker
         private static void PrintBanner()
         {
             ConsoleWriteLineS();
-            ConsoleWriteLineS($"Zelda Ocarina of Time NPC Creation Tool v.3.771 tempCLI dotNET");
+            ConsoleWriteLineS($"Zelda Ocarina of Time NPC Creation Tool v.3.772 tempCLI dotNET");
         }
 
         private static void InitializePaths()
