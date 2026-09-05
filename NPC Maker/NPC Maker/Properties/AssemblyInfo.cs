acdf89f57eb462fc7837dc7f9788295db8a11caf
skawo <43761362+skawo@users.noreply.github.com>
2026-09-05 17:55:39 +0200

    Update AssemblyInfo.cs

--- NPC Maker/NPC Maker/Properties/AssemblyInfo.cs
+++ NPC Maker/NPC Maker/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.774")]
-[assembly: AssemblyFileVersion("3.774")]
+[assembly: AssemblyVersion("3.775")]
+[assembly: AssemblyFileVersion("3.775")]
